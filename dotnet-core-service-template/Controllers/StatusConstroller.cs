f6e6692e58971c31cf2f48a921d91e477688f74e
Hans Kilian <hkkilian@gmail.com>
2017-06-19 13:46:20 +0200

    Added status endpoint

---
+++ dotnet-core-service-template/Controllers/StatusConstroller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace dotnet_core_service_template.Controllers
+{
+    [Route("api/status")]
+    public class StatusController : Controller
+    {
+        // GET api/status
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
--- dotnet-core-service-template/Controllers/ValuesController.cs
+++ dotnet-core-service-template/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_core_service_template.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     public class ValuesController : Controller
     {
         // GET api/values
